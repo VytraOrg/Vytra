70f3362df247cb63b7e4103c89e91191583a26ba
Sayan Pandit <sayanpandit755@gmail.com>
2026-03-25 09:08:28 +0000

    Created app.py and .env

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
+load_dotenv() 
+
+app = Flask(__name__)
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+db = client["local_commerce"]
+
+@app.route("/")
+def home():
+    return "MongoDB Connected ✅"
+
+if __name__ == "__main__":
+    app.run(debug=True)
